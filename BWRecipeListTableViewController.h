935172a9350483df184f65b29f349468b1dfecf9
John Wold <John@Johns-MacBook-Pro.local>
2014-11-05 20:37:57 -0700

    setup initial table view for recipe list

--- BWRecipeListTableViewController.h
+++ BWRecipeListTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BWReciepeListTableViewController.h
 //  Brewseph
 //
 //  Created by John Wold on 11/5/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BWRecipeListTableViewController : UITableViewController
 
 @end
-
