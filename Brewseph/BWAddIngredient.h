f0073bc4513fb859461b85a10e5f13593f88c047
John Wold <askjohnw@yahoo.com>
2014-12-14 17:31:33 -0700

    Add PickerView Spinner

---
+++ Brewseph/BWAddIngredient.h
@@ -0,0 +1,15 @@
+//
+//  BWAddIngredient.h
+//  Brewseph
+//
+//  Created by John Wold on 12/14/14.
+//  Copyright (c) 2014 Brewseph World Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWAddIngredient : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
+@property (strong, nonatomic) NSMutableArray *ounces;
+@property (strong, nonatomic) IBOutlet UIPickerView *ouncesPicker;
+
+@end
---
+++ Brewseph/BWAddIngredient.m
@@ -0,0 +1,56 @@
+//
+//  BWAddIngredient.m
+//  Brewseph
+//
+//  Created by John Wold on 12/14/14.
+//  Copyright (c) 2014 Brewseph World Industries. All rights reserved.
+//
+
+#import "BWAddIngredient.h"
+
+@interface BWAddIngredient ()
+
+@end
+
+@implementation BWAddIngredient
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    self.ounces = [[NSMutableArray alloc] initWithObjects:@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16", nil];
+
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return 16;
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+    
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    NSString *title = [self.ounces objectAtIndex:row];
+    return title;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
